2cd2bb012459ff0faa295cb480a7dc9cbc358496
AlirezaAbavi <abavi.alireza2002@gmail.com>
2022-10-05 02:40:03 +0330

    add authenticate using google

--- DjangoBlog/urls.py
+++ DjangoBlog/urls.py
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from admin_notification.views import check_notification_view
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
@@ -24,6 +23,7 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', Login.as_view(), name='login'),
+    path('', include('social_django.urls', namespace='social')),
     path('register/', Register.as_view(), name='register'),
     path('activate/<uidb64>/<token>/', activate, name='activate'),
     path('', include('django.contrib.auth.urls')),
